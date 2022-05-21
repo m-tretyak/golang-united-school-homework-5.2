9abe28ed8544971b262d2deaa908e092a931ee82
m-tretyak <m.tretyak@gmail.com>
2022-05-21 07:11:06 +0300

    HW52

--- cache.go
+++ cache.go
@@ -1,23 +1,75 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
+	items  map[string]cacheItem
+	locker sync.Locker
 }
 
-func NewCache() Cache {
-	return Cache{}
+type cacheItem struct {
+	value    string
+	deadline time.Time
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (item *cacheItem) isExpired(now time.Time) bool {
+	return item != nil && !item.deadline.IsZero() && item.deadline.Before(now)
 }
 
-func (receiver) Put(key, value string) {
+func NewCache() (result *Cache) {
+	result = new(Cache)
+	result.locker = new(sync.Mutex)
+	result.locker.Lock()
+	defer result.locker.Unlock()
+
+	result.items = make(map[string]cacheItem)
+	return
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Get(key string) (string, bool) {
+	cache.locker.Lock()
+	defer cache.locker.Unlock()
+
+	now := time.Now()
+
+	if item, ok := cache.items[key]; ok && !item.isExpired(now) {
+		return item.value, true
+	} else if ok {
+		delete(cache.items, key)
+	}
+
+	return "", false
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) Put(key, value string) {
+	cache.PutTill(key, value, time.Time{})
+}
+
+func (cache *Cache) Keys() (result []string) {
+	result = make([]string, 0, len(cache.items))
+	now := time.Now()
+
+	for key, item := range cache.items {
+		if item.isExpired(now) {
+			delete(cache.items, key)
+			continue
+		}
+
+		result = append(result, key)
+	}
+
+	return
+}
+
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.locker.Lock()
+	defer cache.locker.Unlock()
+
+	cache.items[key] = cacheItem{
+		value:    value,
+		deadline: deadline,
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module HW52
+
+go 1.18
